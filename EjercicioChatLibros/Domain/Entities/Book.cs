adad20ec2e469881eac9f8642083bfc1df2ab620
Angeles Navarro Zavalia <navarroanguie5@gmail.com>
2025-06-18 22:14:42 -0300

    Add files via upload

---
+++ EjercicioChatLibros/Domain/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    public class Book
+    {
+        public required Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required int Year { get; set; }
+        public required string ISBN { get; set; }
+
+        //relacion 1 a 1 entre libro y categoria
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        //relacion 1 a muchos con prestamo
+        public ICollection<Loan>? Loan { get; set; }
+    }
+}
---
+++ EjercicioChatLibros/Domain/Entities/Loan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Loan
+{
+    public int? Id { get; set; }
+    public DateTime? LoanDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    
+    public int? BookId { get; set; }
+    public Book? Book { get; set; }
+
+    public int? UserId { get; set; }
+    public User? User { get; set; }
+
+
+
+}
